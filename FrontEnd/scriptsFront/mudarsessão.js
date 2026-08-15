de5317301018ddb808671d5ff4746ce1f4068c4a
Bragalucas49X <lucasbraga49x@gmail.com>
2026-08-15 12:22:34 -0300

    Adicionando a parte de cadastro

---
+++ FrontEnd/scriptsFront/mudarsessão.js
@@ -0,0 +1,14 @@
+const rotaCadastro = document.getElementById('rotaCadastro')
+rotaVoltar = document.getElementById('rotaVoltar')
+const formularioLogin = document.getElementById('formLogin')
+const formularioCadastro = document.getElementById('formCadastro')
+
+rotaCadastro.addEventListener(`click`, ()=>{
+    formularioLogin.setAttribute('style', 'display: none;')
+    formularioCadastro.removeAttribute('style')
+})
+
+rotaVoltar.addEventListener('click', ()=>{
+    formularioLogin.removeAttribute('style')
+    formularioCadastro.setAttribute('style', 'display: none;')
+})
