d741f0911cae64bc12b8df75441e19f4168e33de
Bragalucas49X <lucasbraga49x@gmail.com>
2026-08-25 11:10:44 -0300

    Adicionando mais uma tela

--- FrontEnd/scriptsFront/gerarMenuGeral.js
+++ FrontEnd/scriptsFront/gerarMenuGeral.js
@@ -4,48 +4,48 @@ function GerarMenu() {
     header.innerHTML = `
     
      <div class="logo">
-            <img src="FrontEnd/assets/Site assets/LogoOriginal.png" alt="logoEmpresa">
+            <img src="../assets/Site assets/LogoOriginal.png" alt="logoEmpresa">
         </div>
 
         <div class="inputPesquisa">
             <input type="text" placeholder="Busque o produto aqui">
             <div class="icon">
-                <img src="FrontEnd/assets/Site assets/pesquisarIcon.png" alt="lupaIcon">
+                <img src="../assets/Site assets/pesquisarIcon.png" alt="lupaIcon">
             </div>
         </div>
 
         <div class="login">
             <div class="icon">
-                <img src="FrontEnd/assets/Site assets/usuarioVoltaIcon.png" alt="">
+                <img src="../assets/Site assets/usuarioVoltaIcon.png" alt="">
             </div>
-            <a href="FrontEnd/paginas/login.html">Voltar ao login</a>
+            <a href="paginas/login.html">Voltar ao login</a>
         </div>
 
         <div class="menu">
             <button command="show-modal" commandfor="carrinho">
-                <img src="FrontEnd/assets/Site assets/carrinhoIcon.png" alt="Abrir Janela">
+                <img src="../assets/Site assets/carrinhoIcon.png" alt="Abrir Janela">
             </button>
 
             <dialog id="carrinho">
                 <button command="close" commandfor="carrinho">
-                    <img src="FrontEnd/assets/Site assets/closeIcon.png" alt="Fechar"> Fechar
+                    <img src="../assets/Site assets/closeIcon.png" alt="Fechar"> Fechar
                 </button>
             </dialog>
 
             <button command="show-modal" commandfor="links">
-                <img src="FrontEnd/assets/Site assets/menuIcon.png" alt="Abrir Janela">
+                <img src="../assets/Site assets/menuIcon.png" alt="Abrir Janela">
             </button>
 
             <dialog id="links">
                 <button command="close" commandfor="links">
-                    <img src="FrontEnd/assets/Site assets/closeIcon.png" alt="Fechar"> Fechar
+                    <img src="../assets/Site assets/closeIcon.png" alt="Fechar"> Fechar
                 </button>
 
                 <div class="linksMenu">
                     <ul>
-                        <li><a href="">Inicio</a></li>
+                        <li><a href="/Demanda-da-Saga-SENAI/home.html">Inicio</a></li>
                         <li><a href="">Loja</a></li>
-                        <li><a href="FrontEnd/paginas/feedbacks.html">Avaliações</a></li>
+                        <li><a href="../paginas/feedbacks.html">Avaliações</a></li>
                     </ul>
                 </div>
             </dialog>
